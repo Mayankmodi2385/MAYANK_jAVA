2c58d8c8761500c56d55e5fddc621dad1ccc863d
Mayankmodi2385 <mayankmodi7126@gmail.com>
2025-03-27 16:59:13 +0530

    Add files via upload

---
+++ JsonObject.java
@@ -0,0 +1,14 @@
+
+public class JsonObject {
+
+    public JsonObject getAsJsonObject(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAsJsonObject'");
+    }
+
+    public Object get(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+}
---
+++ JsonParser.java
@@ -0,0 +1,9 @@
+
+public class JsonParser {
+
+    public static Object parseString(String content) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parseString'");
+    }
+
+}
